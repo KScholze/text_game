f1d5386a7e493573dbc132b56686f7ab59702e91
KScholze <118296097+KScholze@users.noreply.github.com>
2022-11-15 18:09:57 +0100

    Create game.py

---
+++ game.py
@@ -0,0 +1,11 @@
+
+from text_game.game_logic import *
+
+print(
+    "XXX Willkommen im Dungeon XXX\n"
+    "Du bist in einem dunklen Verließ und musst einen Schatz suchen\n"
+    "Doch du bist nicht alleine. Sei auf der Hut...\n"
+    f"Deine aktuelle Position: X:{spieler.x_pos} | Y:{spieler.y_pos}"
+)
+
+spiel()
